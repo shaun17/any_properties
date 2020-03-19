d17c1dd633cf8597cfb4aee4cef851cc2b36f10d
shaun <shaun.17@outlook.com>
2020-03-19 15:24:37 +0800

    a

---
+++ src/main/java/sw/java/elk/demo/syn/Synchro.java
@@ -0,0 +1,15 @@
+package sw.java.elk.demo.syn;
+
+public class Synchro {
+     Object o = new Object();
+    public static void main(String[] args) {
+
+        TH t1 = new TH();
+        TH t2 = new TH();
+        TH t3 = new TH();
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+}
